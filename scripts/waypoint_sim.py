928aec97662d7855fe8f927ee3d2db1e4bfc6431
reapur <60037499+itsmoihemanth@users.noreply.github.com>
2023-10-17 15:30:34 -0400

    Update waypoint_sim.py

--- scripts/waypoint_sim.py
+++ scripts/waypoint_sim.py
@@ -3,41 +3,44 @@
 import rospy
 from geometry_msgs.msg import PoseStamped
 
-def send_goal(x, y, yaw):
-    goal = PoseStamped()
-    goal.header.frame_id = "map"
-    goal.pose.position.x = x
-    goal.pose.position.y = y
-    goal.pose.orientation.z = yaw
-    goal.pose.orientation.w = 1.0
+def send_goal():
 
-    goal_pub = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=10)
-    
-    rate = rospy.Rate(10)  # Publish at 10 Hz
-    timeout = 30  # 30 seconds timeout
+    waypoints = [
+        (1.0, 0.0, 0.0),
+        (0.0, 1.0, 0.0)
+    ]
 
-    start_time = rospy.get_time()
-    while (rospy.get_time() - start_time) < timeout:
-        goal_pub.publish(goal)
-        rate.sleep()
+    for waypoint in waypoints:
+        x, y, yaw = waypoint
+        goal = PoseStamped()
+        goal.header.frame_id = "map"
+        goal.pose.position.x = x
+        goal.pose.position.y = y
+        goal.pose.orientation.z = yaw
+        goal.pose.orientation.w = 1.0
+
+        goal_pub = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=10)
+
+        rate = rospy.Rate(10)  # Publish at 10 Hz
+        timeout = 30  # 30 seconds timeout
+
+        start_time = rospy.get_time()
+        while (rospy.get_time() - start_time) < timeout:
+            goal_pub.publish(goal)
+            rate.sleep()
 
 def main():
-    try:
-        rospy.init_node('waypoint_planner', anonymous=True)
-        
-        # Define your waypoints (x, y, yaw)
-        waypoints = [
-            (1.0, 0.0, 0.0),
-            (0.0,1.0,0.0)
-            ]
-
-        for waypoint in waypoints:
-            x, y, yaw = waypoint
-            send_goal(x, y, yaw)
-        
-    except rospy.ROSInterruptException:
-        pass
+    rospy.init_node('waypoint_node_sim', anonymous=True)
+    rospy.spin()
+    send_goal(x, y, yaw)
 
 if __name__ == '__main':
     main()
 
+
+
+odof 
+/tf - transformations
+	- matrix
+amcl - /base
+/map
