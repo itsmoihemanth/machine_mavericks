e4be155b370c73c9a4471050c18b03c1ebf18d70
reapur <60037499+itsmoihemanth@users.noreply.github.com>
2023-10-24 16:11:35 -0400

    Update grasp_object.py

--- scripts/grasp_object.py
+++ scripts/grasp_object.py
@@ -4,7 +4,7 @@ import rospy
 from geometry_msgs.msg import PointStamped
 
 def move_arm(x, y, z):
-    pub = rospy.Publisher('/stretch_arm_controller/follow_joint_trajectory/goal', PointStamped, queue_size=10)
+    pub = rospy.Publisher('/clicked_point', PointStamped, queue_size=10)
     goal = PointStamped()
     goal.header.frame_id = "map"
     goal.point.x = x
@@ -27,12 +27,12 @@ def main():
     rospy.init_node('grasp_object_node')
     
     # Move the arm up and down
-    move_arm(1.0, 1.0, 1.0)  # Move up (adjust the position as needed)
-    rospy.sleep(2)  # Adjust the duration 
+    move_arm(1.384, 2.156, 0.5)  # Move up (adjust the position as needed)
+    rospy.sleep(30)  # Adjust the duration 
     
     # Open and close the gripper
     move_gripper(1.0, 1.0, 0.0)  # Open the gripper (adjust the positions as needed)
-    rospy.sleep(2)  # Adjust the duration
+    rospy.sleep(30)  # Adjust the duration
     
     move_gripper(0.0, 0.0, 0.0)  # Close the gripper (adjust the positions as needed)
     
