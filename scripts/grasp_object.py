6cc54e949ac26cf3135db0c7be76bc5d96b25b82
reapur <60037499+itsmoihemanth@users.noreply.github.com>
2023-11-16 10:04:53 -0500

    Add files via upload

--- scripts/grasp_object.py
+++ scripts/grasp_object.py
@@ -1,42 +1,48 @@
 #!/usr/bin/env python3
 
 import rospy
-from geometry_msgs.msg import PointStamped
+import actionlib
+from control_msgs.msg import FollowJointTrajectoryAction, FollowJointTrajectoryGoal
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 
-def move_arm(x, y, z):
-    pub = rospy.Publisher('/clicked_point', PointStamped, queue_size=10)
-    goal = PointStamped()
-    goal.header.frame_id = "map"
-    goal.point.x = x
-    goal.point.y = y
-    goal.point.z = z
-    print("Publishing arm goal:", goal)  # Print the data
-    pub.publish(goal)
+def move_arm_to_waypoints(waypoints):
+    rospy.init_node('stretch_arm_actionlib', anonymous=True)
 
-def move_gripper(x, y, z):
-    pub_left = rospy.Publisher('/stretch_gripper_controller/follow_joint_trajectory/goal', PointStamped, queue_size=10)
-    goal = PointStamped()
-    goal.header.frame_id = "map"
-    goal.point.x = x
-    goal.point.y = y
-    goal.point.z = z
-    print("Publishing gripper goal:", goal)  # Print the data
-    pub_left.publish(goal)
+    client = actionlib.SimpleActionClient('/stretch_arm_controller/follow_joint_trajectory', FollowJointTrajectoryAction)
+    client.wait_for_server()
 
-def main():
-    rospy.init_node('grasp_object_node')
-    
-    # Move the arm up and down
-    move_arm(1.384, 2.156, 0.5)  # Move up (adjust the position as needed)
-    rospy.sleep(30)  # Adjust the duration 
-    
-    # Open and close the gripper
-    move_gripper(1.0, 1.0, 0.0)  # Open the gripper (adjust the positions as needed)
-    rospy.sleep(30)  # Adjust the duration
-    
-    move_gripper(0.0, 0.0, 0.0)  # Close the gripper (adjust the positions as needed)
-    
-    rospy.spin()
+    # Define the arm joint names
+    arm_joint_names = ["joint_wrist_pitch", "joint_gripper_finger_left", "joint_gripper_finger_right"]
+
+    for waypoint in waypoints:
+        trajectory_msg = JointTrajectory()
+        trajectory_msg.joint_names = arm_joint_names
+
+        point = JointTrajectoryPoint()
+        point.positions = waypoint
+        point.time_from_start = rospy.Duration(2.0)  # Adjust the duration as needed
+
+        trajectory_msg.points.append(point)
+
+        goal = FollowJointTrajectoryGoal()
+        goal.trajectory = trajectory_msg
+
+        # Send the trajectory goal to move the arm
+        client.send_goal(goal)
+
+        # Wait for the arm to reach the waypoint
+        client.wait_for_result()
 
 if __name__ == '__main__':
-    main()
+    try:
+        waypoints = [
+            [0.1, 0.2, 0.3],  # Joint positions for waypoint 1
+            [0.4, 0.5, 0.6],  # Joint positions for waypoint 2
+            [0.7, 0.8, 0.9],  # Joint positions for waypoint 3
+        ]
+
+        move_arm_to_waypoints(waypoints)
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
+
